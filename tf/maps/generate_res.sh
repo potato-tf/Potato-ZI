720f1b76f8c9d349e738a1ee62abce55a373255a
Your Name <40523890+Brain-dawg@users.noreply.github.com>
2025-10-03 19:30:13 -0400

    fix

--- tf/maps/generate_res.sh
+++ tf/maps/generate_res.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ -f ../cfg/mapcycle_zi.txt ]; then
-    while IFS= read -r mapname || [ -n "$mapname" ]; do 
-        cp -au ../zi/cfg/downloads_zi.kv $mapname.res; 
+    while IFS= read -r mapname; do
+        cp -au ../zi/cfg/downloads_zi.kv ./$mapname.res
     done < ../cfg/mapcycle_zi.txt
 else
     echo "mapcycle_zi.txt not found"
